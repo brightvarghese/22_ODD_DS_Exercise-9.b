223f93557738d482c0950003469e7331fa2c2530
brightvarghese <77091625+brightvarghese@users.noreply.github.com>
2022-09-27 18:48:37 +0530

    Initial commit for code template

---
+++ Main.py
@@ -0,0 +1,14 @@
+from typing import List
+
+def quick_sort(data, low, high) -> List[int]:
+    # Write code here
+
+
+input_data = input()
+data = []
+for item in input_data.split(', '):
+  if item.isnumeric():
+    data.append(int(item))
+  elif item.lstrip("-").isnumeric():
+    data.append(int(item))
+print(quick_sort(data, 0, len(data)-1))
